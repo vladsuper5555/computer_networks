6d05ee76e31e34b011c41973e63bc09283f66298
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2023-12-03 20:42:00 +0200

    Update requester.c

--- requester.c
+++ requester.c
@@ -6,6 +6,16 @@
 #include <unistd.h>
 #include <string.h>
 
+void extractProtocol(char *url, char *protocol) {
+    char *protoEnd = strstr(url, "://");
+    if (protoEnd == NULL) {
+        strcpy(protocol, "http"); // Default to HTTP if no protocol specified
+    } else {
+        strncpy(protocol, url, protoEnd - url);
+        protocol[protoEnd - url] = '\0';
+    }
+}
+
 void extractDomain(char *url, char *domain) {
     char *start = strstr(url, "://");
     if (start == NULL) {
@@ -45,19 +55,25 @@ int main(int argc, char *argv[]) {
 
     char buf[2056];
     char url[2056];
-    strcpy(url, argv[1]); // Using argv[1] as argv[0] is the program name
+    char protocol[10];
+    strcpy(url, argv[1]);
     printf("the url given is %s\n", url);
-    int byte_count;
+
+    extractProtocol(url, protocol);
+    printf("Protocol: %s\n", protocol);
 
     char domain[2056];
     char path[2056];
     extractDomain(url, domain);
     extractPath(url, path);
-    printf("%s %s \n", path, domain);
+    printf("Path: %s, Domain: %s\n", path, domain);
+
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(domain, "80", &hints, &res);
+    // Set the port based on the protocol
+    const char *port = strcmp(protocol, "https") == 0 ? "443" : "80";
+    getaddrinfo(domain, port, &hints, &res);
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     printf("Connecting...\n");
     connect(sockfd, res->ai_addr, res->ai_addrlen);
@@ -68,7 +84,7 @@ int main(int argc, char *argv[]) {
     send(sockfd, header, strlen(header), 0);
     printf("GET Sent...\n");
 
-    byte_count = recv(sockfd, buf, sizeof buf, 0);
+    int byte_count = recv(sockfd, buf, sizeof buf, 0);
     printf("recv()'d %d bytes of data in buf\n", byte_count);
     printf("%s", buf);
 
