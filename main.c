cf1a6e1774ce4d7d47208b534d1456c6294e11ca
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2023-12-03 23:53:26 +0200

    Update main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "./webcontent.h"
+#include <stdio.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[]) {
+    char result[10000];
+    // check the args first time
+    returnFilesContent(argv[1], result);
+
+    const char *root = "root";
+    printf("the final lenfth is %d \n", strlen(result));
+    printf("%s\n", result);
+    return 0;
+}
