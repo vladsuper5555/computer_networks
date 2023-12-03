6646796f4e4cae72e232babc5c0c40b85904de71
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2023-12-03 21:52:10 +0200

    refactored the code a bit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <webcontent.h>
+
+
+int main(int argc, char *argv[]) {
+    char result[100000];
+    // check the args first time
+    returnFilesContent(argv[1], result);
+
+    printf("%s\n", result);
+    return 0;
+}
---
+++ webcontent.c
@@ -0,0 +1,94 @@
+#include "webcontent.h"
+
+static void extractProtocol(const char *url, char *protocol);
+static void extractDomain(const char *url, char *domain);
+static void extractPath(const char *url, char *path);
+
+void returnFilesContent(const char *url, char *result) {
+    struct addrinfo hints, *res;
+    int sockfd;
+    char buf[10056];
+    char protocol[10];
+    char domain[2056];
+    char path[2056];
+
+    extractProtocol(url, protocol);
+    extractDomain(url, domain);
+    extractPath(url, path);
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    const char *port = strcmp(protocol, "https") == 0 ? "443" : "80";
+    
+    if (getaddrinfo(domain, port, &hints, &res) != 0) {
+        perror("getaddrinfo failed");
+        return;
+    }
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
+        perror("Connection failed");
+        close(sockfd);
+        freeaddrinfo(res);
+        return;
+    }
+
+    char header[5128];
+    sprintf(header, "GET %s \r\nHost: %s\r\n\r\n", path, domain);
+    send(sockfd, header, strlen(header), 0);
+
+    while (1) {
+        int byte_count = recv(sockfd, buf, sizeof buf, 0);
+        if (byte_count <= 0) {
+            break;
+        }
+        strcat(result, buf);
+    }
+
+    close(sockfd);
+    freeaddrinfo(res);
+}
+
+void extractProtocol(char *url, char *protocol) {
+    char *protoEnd = strstr(url, "://");
+    if (protoEnd == NULL) {
+        strcpy(protocol, "http"); // Default to HTTP if no protocol specified
+    } else {
+        strncpy(protocol, url, protoEnd - url);
+        protocol[protoEnd - url] = '\0';
+    }
+}
+
+void extractDomain(char *url, char *domain) {
+    char *start = strstr(url, "://");
+    if (start == NULL) {
+        start = url;
+    } else {
+        start += 3;
+    }
+
+    char *end = strchr(start, '/');
+    if (end == NULL) {
+        strcpy(domain, start);
+    } else {
+        strncpy(domain, start, end - start);
+        domain[end - start] = '\0';
+    }
+}
+
+void extractPath(char *url, char *path) {
+    char *start = strstr(url, "://");
+    if (start == NULL) {
+        start = url;
+    } else {
+        start += 3;
+    }
+
+    char *pathStart = strchr(start, '/');
+    if (pathStart == NULL) {
+        strcpy(path, "/");
+    } else {
+        strcpy(path, pathStart);
+    }
+}
