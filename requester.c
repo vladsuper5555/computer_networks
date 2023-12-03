8bb2d223690391a3a3f135418132e98be2b2740c
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2023-12-03 20:33:52 +0200

    Update requester.c

--- requester.c
+++ requester.c
@@ -1,86 +1,79 @@
-#include <stdio.h> /* printf, sprintf */
-#include <stdlib.h> /* exit */
-#include <unistd.h> /* read, write, close */
-#include <string.h> /* memcpy, memset */
-#include <sys/socket.h> /* socket, connect */
-#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
-#include <netdb.h> /* struct hostent, gethostbyname */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-void error(const char *msg) { perror(msg); exit(0); }
+void extractDomain(char *url, char *domain) {
+    char *start = strstr(url, "://");
+    if (start == NULL) {
+        start = url;
+    } else {
+        start += 3;
+    }
 
-int main(int argc,char *argv[])
-{
-    /* first what are we going to send and where are we going to send it? */
-    int portno =        80;
-    const char *host =        "profs.info.uaic.ro";
-    const char *message_fmt = "  GET /~olariu HTTP/1.0\r\n\r\n";
+    char *end = strchr(start, '/');
+    if (end == NULL) {
+        strcpy(domain, start);
+    } else {
+        strncpy(domain, start, end - start);
+        domain[end - start] = '\0';
+    }
+}
 
-    struct hostent *server;
-    struct sockaddr_in serv_addr;
-    int sockfd, bytes, sent, received, total;
-    char message[1024],response[4096];
+void extractPath(char *url, char *path) {
+    char *start = strstr(url, "://");
+    if (start == NULL) {
+        start = url;
+    } else {
+        start += 3;
+    }
 
+    char *pathStart = strchr(start, '/');
+    if (pathStart == NULL) {
+        strcpy(path, "/");
+    } else {
+        strcpy(path, pathStart);
+    }
+}
 
-    /* fill in the parameters */
-    // sprintf(message,message_fmt,argv[1],argv[2]);
-    printf("Request:\n%s\n",message);
+int main(int argc, char *argv[]) {
+    struct addrinfo hints, *res;
+    int sockfd;
 
-    /* create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("ERROR opening socket");
+    char buf[2056];
+    char url[2056];
+    strcpy(url, argv[0]); // Using argv[1] as argv[0] is the program name
+    int byte_count;
 
-    /* lookup the ip address */
-    server = gethostbyname(host);
-    if (server == NULL) error("ERROR no such host");
+    char domain[2056];
+    char path[2056];
+    extractDomain(url, domain);
+    extractPath(url, path);
 
-    /* fill in the structure */
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portno);
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    getaddrinfo(domain, "80", &hints, &res);
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    printf("Connecting...\n");
+    connect(sockfd, res->ai_addr, res->ai_addrlen);
+    printf("Connected!\n");
 
-    /* connect the socket */
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
+    char header[2056];
+    sprintf(header, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
+    send(sockfd, header, strlen(header), 0);
+    printf("GET Sent...\n");
 
-    /* send the request */
-    total = strlen(message);
-    sent = 0;
-    do {
-        bytes = write(sockfd,message+sent,total-sent);
-        if (bytes < 0)
-            error("ERROR writing message to socket");
-        if (bytes == 0)
-            break;
-        sent+=bytes;
-    } while (sent < total);
+    byte_count = recv(sockfd, buf, sizeof buf, 0);
+    printf("recv()'d %d bytes of data in buf\n", byte_count);
+    printf("%s", buf);
 
-    /* receive the response */
-    memset(response,0,sizeof(response));
-    total = sizeof(response)-1;
-    received = 0;
-    do {
-        bytes = read(sockfd,response+received,total-received);
-        if (bytes < 0)
-            error("ERROR reading response from socket");
-        if (bytes == 0)
-            break;
-        received+=bytes;
-    } while (received < total);
-
-    /*
-     * if the number of received bytes is the total size of the
-     * array then we have run out of space to store the response
-     * and it hasn't all arrived yet - so that's a bad thing
-     */
-    if (received == total)
-        error("ERROR storing complete response from socket");
-
-    /* close the socket */
+    // Close the socket
     close(sockfd);
-
-    /* process response */
-    printf("Response:\n%s\n",response);
+    freeaddrinfo(res);
 
     return 0;
 }
