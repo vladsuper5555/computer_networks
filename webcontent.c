49b0d2a1dbed75d31fcfef2583721eeec4b86fc2
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2023-12-04 00:05:08 +0200

    Update webcontent.c

--- webcontent.c
+++ webcontent.c
@@ -81,7 +81,7 @@ void returnFilesContent(char *url, char *result) {
     }
 
     char header[5128];
-    sprintf(header, "GET %s \r\nHost: %s\r\n\r\n", path, domain);
+    sprintf(header, "GET %s HTTP1.1/ \r\nHost: %s\r\n\r\n", path, domain);
     send(sockfd, header, strlen(header), 0);
 
     while (1) {
