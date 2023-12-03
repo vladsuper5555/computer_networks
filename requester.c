7d642d15091294de8bf09e69d86ee62fe8131fd7
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2023-12-03 21:17:35 +0200

    Update requester.c

--- requester.c
+++ requester.c
@@ -87,11 +87,11 @@ int main(int argc, char *argv[]) {
     {
         int byte_count = recv(sockfd, buf, sizeof buf, 0);
         printf("recv()'d %d bytes of data in buf\n", byte_count);
-        printf("%s", buf);
+        // printf("%s", buf);
 
         // Close the socket
-        close(sockfd);
-        freeaddrinfo(res);
     }
+    close(sockfd);
+    freeaddrinfo(res);
     return 0;
 }
